93806543ac2517d2024704a67b3028394012b2d7
Sergio Lejarde <lejardesergio17@gmail.com>
2025-09-03 00:46:26 -0500

    commit: vuelvo a la opcion que me estaba sirviendo en github actions, aun falta arreglar o darle otro camino al job del ipa

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';//
+import 'package:url_launcher/url_launcher.dart';//-
 
 void main() {
   runApp(const PawCampusApp());
